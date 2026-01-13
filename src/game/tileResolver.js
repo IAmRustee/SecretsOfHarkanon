fc348e61c91e0082485a6e6a95ce0e359a7064bf
IAmRustee <wewen305@gmail.com>
2026-01-13 10:38:44 +0800

    updated sprites

--- src/game/tileResolver.js
+++ src/game/tileResolver.js
@@ -1,10 +1,13 @@
 export function getTileSprite(symbol) {
   switch (symbol) {
     case "#": return "/tiles/wall/wall_1.png";
+    case "b": return "/tiles/objects/blocker_inactive_vert.png";
     case "3": return "/tiles/wall/wall_1_top.png";
     case ".": return "/tiles/floor/tile_1.png";
     case "P": return "/tiles/objects/pressure_plate_1.png";
+    case "f": return "/tiles/objects/pressure_plate_2.png";
     case "G": return "/tiles/gate/gate_closed.png";
+    case "?": return "/tiles/placeholder.png"
     default: return null;
   }
 }
