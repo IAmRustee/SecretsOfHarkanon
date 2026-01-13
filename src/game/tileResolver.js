1165468791ce6f15646c79c5273ea7bc7dc103f8
IAmRustee <wewen305@gmail.com>
2026-01-13 10:03:15 +0800

    Mapdata update 1

--- src/game/tileResolver.js
+++ src/game/tileResolver.js
@@ -1,6 +1,7 @@
 export function getTileSprite(symbol) {
   switch (symbol) {
-    case "#": return "/tiles/wall/wall_2.png";
+    case "#": return "/tiles/wall/wall_1.png";
+    case "3": return "/tiles/wall/wall_1_top.png";
     case ".": return "/tiles/floor/tile_1.png";
     case "P": return "/tiles/objects/pressure_plate_1.png";
     case "G": return "/tiles/gate/gate_closed.png";
